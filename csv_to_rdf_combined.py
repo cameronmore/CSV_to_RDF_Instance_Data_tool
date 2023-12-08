8b84f294bc33cf155d080329040918fea9cf86af
Cameron More <123985185+cameronmore@users.noreply.github.com>
2023-12-08 09:07:40 -0500

    More progress on turning string manipulation into rdflib syntax

--- csv_to_rdf_combined.py
+++ csv_to_rdf_combined.py
@@ -17,70 +17,77 @@ owl = Namespace('http://www.w3.org/2002/07/owl#')
 g.bind('owl',owl)
 
 
-def process_mapping(mapping_file,uuid_or_hex):
+def total_mapping_and_generate_data(mapping_file,data_file,uuid_or_hex):
     """
     Iterate through the 
         
     """
-
-    
-    #Create a temporary_mapping working file to read and write to
-    final_file = 'temporary_mapping'
-    with open(os.path.join(os.getcwd(), final_file), 'w') as f:
-        f.write('')
-
-    #Change IRIs to RDFLib-readable IRIs to add
-    with open(mapping_file, 'r') as input_file, open(final_file, 'w', newline='') as output_file:
-        reader = csv.DictReader(input_file)
-        writer = csv.DictWriter(output_file, fieldnames=reader.fieldnames)
-        writer.writeheader()
-
-        for row in reader:
+    with open(data_file, 'r') as f:
+        data_reader = csv.DictReader(f)
+        for data_row in data_reader:
             
+            # Generate UUID for each row
             uuid_str = str(uuid.uuid4())
             if uuid_or_hex is False:
                 uuid_str = uuid.uuid4().hex
-         
-            if row['p'] != 'RDF.type' :
-                if 'datapoint' in row['o']:
-                    g.add((URIRef(row['s']+uuid_str),RDF.type,Literal(    )   ))
-                    
-                    #Do the adding and iterating in this step here!!
-                    
-                    #Enumerate the possible triple options:
-                        # if instance , rdf:type , class
-                            #then x
-                        # if instance , objectproperty , instance
-                            #then y
-                        # if instance , object property , datapoint
-                            #then z
-                    
-                    #todo for long term, add possible pyTransform so literals can be bound as certain iris? too diffcult to do in systematic form?
-                    
-                    row['s'] = str("URIRef(" + row['s'] + " + uuid_str)")
-                elif 'datapoint' in row['s']:
-                    row['o'] = str("URIRef(" + row['o'] + " + uuid_str)")   
-                else:
-                    row['s'] = str("URIRef(" + row['s'] + " + uuid_str)")
-                    row['o'] = str("URIRef(" + row['o'] + " + uuid_str)")
-            if row['p'] == 'RDF.type':
-                row['s'] = str("URIRef(" + row['s'] + " + uuid_str)")           
-                new_row = {'s': row['s'], 'p': row['p'], 'o': 'owl.NamedIndividual'}
-                writer.writerow(new_row)
+
+        
+
+
+        #Change IRIs to RDFLib-readable IRIs to add
+        with open(mapping_file, 'r') as input_file:
+            mapping_reader = csv.DictReader(input_file)
+
+            for mapping_row in mapping_reader:
                 
-            writer.writerow(row)
+                uuid_str = str(uuid.uuid4())
+                if uuid_or_hex is False:
+                    uuid_str = uuid.uuid4().hex
+                    
+                if mapping_row['p'] == 'RDF.type' :
+                    g.add((URIRef(mapping_row['s']+uuid),RDF.type,mapping_row['o']))
+                    g.add((URIRef(mapping_row['s']+uuid),RDF.type,owl:NamedIdividual))
 
-    #Read the mapping file and fill in the datapoints with RDFLib-readable IRIs
-    df = pd.read_csv(final_file)
+                    
+                    #Running into an issue above,
+                        #The hard-coded g.add statements need to reference the proper locations in the DATA file, not 's' 'p' 'o' like the lines do above currently
+                        #Am I wrong about that?
+                        #I am wrong about that in this case, the only place it matters is hen we get to the literals!!                
+            
+                if mapping_row['p'] != 'RDF.type' :
+                    if 'datapoint' in mapping_row['o']:
+                        split_datapoint_list = mapping_row['o'].split('/')
+                        #This is where we break the datapoint/string/column down to use in the g.add line below
+                        g.add((URIRef(mapping_row['s']+uuid_str),RDF.type,Literal(row[split_datapoint_list[2]],datatype=XSD.split_datapoint_list[1] )  ))
+                        
+                        #There are a finite amount of kinds of triples this tool can use:
+                        # instance to class
+                        # instance to instance
+                        # instance to datapoint
+                        # It is bad practice to make literals the s in any graph
+                        
+                        #Each triple combination adds the following to the graph:
+                            # if instance , rdf:type , class
+                                #then add uri(instance) rdf:type class
+                                #then add rdf:type owl:NamedIdividual
+                            # if instance , objectproperty , instance
+                                #then add uri(instance) , objectproperty , uri(instance)
+                            # if instance , object property , datapoint
+                                #then add uri(instance) , object proprty , literal(datapoint)
+                        
+                        #The decision tree has two points that make it easy to distinguish which case a row is:
+                            #Either a row in the mapping has datapoint in it or not.
+                            #Either a row in the mapping has rdf:type or not.
+                            #These two options are mutually exclusive
+                    else:
+                        row['s'] = str("URIRef(" + row['s'] + " + uuid_str)")
+                        row['o'] = str("URIRef(" + row['o'] + " + uuid_str)")
 
-    for index, row in df.iterrows():
-        if 'datapoint' in row.iloc[2]:
-            my_list = row.iloc[2].split('/')
-            new_value = 'Literal(row[' + "'" + my_list[2] + "'" + '], datatype=XSD.' + my_list[1] +')'
-            row.iloc[2]=new_value
 
-        if 'datapoint' in row.iloc[0]:
-            my_list = row.iloc[0].split('/')
-            new_value = 'Literal(row[' + "'" + my_list[2] + "'" + '], datatype=XSD.' + my_list[1] +')'
-            row.iloc[0]=new_value
+        for index, row in df.iterrows():
+            if 'datapoint' in row.iloc[2]:
+                my_list = row.iloc[2].split('/')
+                new_value = 'Literal(row[' + "'" + my_list[2] + "'" + '], datatype=XSD.' + my_list[1] +')'
+                row.iloc[2]=new_value
+
 
