e83389bfc3215984648b5dbbdc1d0f1ab3f1bd42
Cameron More <123985185+cameronmore@users.noreply.github.com>
2023-12-08 14:37:03 -0500

    Rearranged repo, added comments, fixed prefix problem, created another problem
    Changed the mapping syntax to use ':' rather than '.' and therefore fixed issue where rdflib wasn't adding prefixes to the graph... but when I fixed that, I found out the correct prefixes weren't being bound properly, so 'ns2' was being used not 'cco'. This is an annoying problem but at least now valid rdf is being generated unlike before. Also added comments to code

--- csv_to_rdf_combined.py
+++ csv_to_rdf_combined.py
@@ -20,13 +20,17 @@ g.bind('owl',owl)
 def total_mapping_and_generate_data(mapping_file,data_file,uuid_or_hex):
     """
     Takes a mapping CSV file and data CSV file as input. \n
+    This function uses a sligtly different syntax than the other version of this tool, it uses ':' rather than '.' between the prefixes and the identifiers. \n
+    Rather than cco.Person, which is RDFLib syntax, one should write cco:Person, which is valid RDF syntax. \n
     Mapping syntax is a simpler version of regular RDFLib syntax. \n
     If uuid_or_hex is False, then the generated triples are generated without hyphens (as hexidecibles, not uuids). If True or None, then regular random uuids will be used. \n
     Native Python modules csv, re, and uuid must be imported. \n
     Dependencies: RDFLib, XSD, and RDF must be imported, a grah must be parsed in RDFLib, the function adds triples to this graph, then serializes and prints the generated data to the terminal. \n
+    Known issues: pre-defined namespaces are not definable. For some reason, ns1 or ns2 are being bound to all the IRIs, not cco. \n
     
         
     """
+   
     with open(data_file, 'r') as f:
         data_reader = csv.DictReader(f)
         for data_row in data_reader:
@@ -40,45 +44,35 @@ def total_mapping_and_generate_data(mapping_file,data_file,uuid_or_hex):
                     uuid_str = str(uuid.uuid4())
                     if uuid_or_hex is False:
                         uuid_str = uuid.uuid4().hex
-                        
-                    if mapping_row['p'] == 'RDF.type' :
-                        g.add((URIRef(mapping_row['s']+uuid_str),RDF.type,URIRef(mapping_row['o'])))
-                        g.add((URIRef(mapping_row['s']+uuid_str),RDF.type,owl.NamedIdividual))
+                    
+                    #The script iterates both the mapping and the data creating triples based off the kind of relation asserted in the mapping:
+                    #Every line is either:
+                        # instance to class
+                        #instance to datapoint
+                    # or
+                        # instsnce to instance         
+
+                    #Case 1: Instance to Class
+                    if mapping_row['p'] == 'RDF:type' :
+                        g.add((URIRef(URIRef(mapping_row['s'])+uuid_str),RDF.type,URIRef(mapping_row['o'])))
+                        g.add((URIRef(URIRef(mapping_row['s'])+uuid_str),RDF.type,owl.NamedIdividual))
                
-                    if mapping_row['p'] != 'RDF.type' :
+                    #Case 1.5 Instance to something else
+                    if mapping_row['p'] != 'RDF:type' :
+                        
+                        #Case 2: Instance to datapoint
                         if 'datapoint' in mapping_row['o']:
                             split_datapoint_list = mapping_row['o'].split('/')
-                            #This is where we break the datapoint/string/column down to use in the g.add line below
-                            g.add((URIRef(mapping_row['s']+uuid_str),RDF.type,Literal(data_row[split_datapoint_list[2]],datatype=XSD.split_datapoint_list[1] )  ))
-                            
-                            #There are a finite amount of kinds of triples this tool can use:
-                            # instance to class
-                            # instance to instance
-                            # instance to datapoint
-                            # It is bad practice to make literals the s in any graph
-                            
-                            #Each triple combination adds the following to the graph:
-                                # if instance , rdf:type , class
-                                    #then add uri(instance) rdf:type class
-                                    #then add rdf:type owl:NamedIdividual
-                                # if instance , objectproperty , instance
-                                    #then add uri(instance) , objectproperty , uri(instance)
-                                # if instance , object property , datapoint
-                                    #then add uri(instance) , object proprty , literal(datapoint)
-                            
-                            #The decision tree has two points that make it easy to distinguish which case a row is:
-                                #Either a row in the mapping has datapoint in it or not.
-                                #Either a row in the mapping has rdf:type or not.
-                                #These two options are mutually exclusive
-                                
+
+                            g.add((URIRef(URIRef(mapping_row['s'])+uuid_str),URIRef(mapping_row['p']),Literal(data_row[split_datapoint_list[2]],datatype=URIRef("http://www.w3.org/2001/XMLSchema#" + split_datapoint_list[1]) )  ))
+                        
+                        #Case 3: Instance to Instance
                         else:
-                            g.add(( URIRef(mapping_row['s']+uuid_str),URIRef(mapping_row['p']),URIRef(mapping_row['o']+uuid_str) ))
+                            g.add(( URIRef(URIRef(mapping_row['s'])+uuid_str),URIRef(mapping_row['p']),URIRef(URIRef(mapping_row['o'])+uuid_str) ))
+
         print(g.serialize(format="turtle"))
 
 
 total_mapping_and_generate_data('sample_mapping.csv','sample_data.csv',True)
 
-#Currently gettinh an error because the mapping file is a csv, and each value of each column is a string, not an RDFLib object
-    #Even though cco.Person is an RDF:Lib object in python, the function is reading it as a string in a csv... which it is...
-#To change this, the mapping could be rewritten in as a python file or written AS a csv but with a .py extension
-#Fixed this problem by just wrapping all the items retrieved from the mapping file with URIRef()
+#Current error: predefined namespaces are not appearing in the generated triples
